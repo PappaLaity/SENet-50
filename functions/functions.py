1af7a560fa6eeb6f0d04a353777ef42053afe8f3
Pappalaity <pappalaity@gmail.com>
2025-08-25 15:59:00 +0000

    define train and test function

---
+++ functions/functions.py
@@ -0,0 +1,69 @@
+import torch
+from models.generated_v2 import SENet50
+
+
+def senet50(num_classes=1, reduction=16):
+    return SENet50(num_classes=num_classes, reduction=reduction)
+
+
+def train(model,device,epochs,dataloader,criterion,optimizer):
+    # Train Mode
+    model.train()
+
+    # Use Gpu or CPU depending of what resource we have
+    model.to(device)
+    
+    # Exponential moving Average loss
+    ema_loss = None
+
+    # For each epochs
+    for epoch in range(epochs):
+        # For Each Batch in dataloader
+        for batch_idx, (data,target) in enumerate(dataloader):
+            
+            # Convert it to device
+            data.to(device)
+            target.to(device)
+            
+            # Train Data Forward Pass
+            output = model(data)
+            
+            # Calculate loss
+            loss = criterion(output.to(device),target)
+
+            # Backward Pass
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if ema_loss == None:
+                ema_loss = loss.item()
+            else:
+                ema_loss+= (loss.item() - ema_loss)*0.01
+            
+        print("Train Epoch {} \t loss: {:.6f}".format(epoch,ema_loss))
+
+
+def evaluate(model,dataloader,device):
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+        for data,target in dataloader:
+            
+            data.to(device)
+
+            output = model(data)
+
+            pred = output.argmax(dim =1 ,keepDim = True)
+
+            correct += pred.cpu().eq(target.view_as(pred)).sum().item()
+
+    percent = (correct*100)/len(dataloader.dataset)
+
+    print(f"Accuracy: {correct} / {len(dataloader.dataset)} ({percent:.2f}%)")
+
+    return percent
+
+def test():
+    pass
