468cc49e3efcde4443e88869142bf243b8281982
Pappalaity <pappalaity@gmail.com>
2025-08-25 15:58:47 +0000

    Prepare cifiar-10  datasets (len 50k)

---
+++ datasets/readme.md
@@ -0,0 +1,9 @@
+## Datasets 
+
+### CIFAR 10
+
+You should download your CIFAR-10 training dataset using this 
+
+[Cifar-10 Datasets](https://www.kaggle.com/competitions/cifar-10/data)
+
+You can unzip in the datasets folder as cifar-10
--- models/dataset.py
+++ models/dataset.py
@@ -1,2 +1,31 @@
-class Dataset():
-    pass
+import pandas as pd
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+import os
+
+class CifarImageDataset(Dataset):
+    def __init__(self, csv_file, image_folder, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.image_folder = image_folder
+        self.transform = transform
+        self.label2idx = {label: idx for idx, label in enumerate(self.annotations.iloc[:,1].unique())}
+
+
+    def __len__(self):
+        return len(self.annotations)
+    
+    def __label2idx__(self):
+        print(self.label2idx)
+
+    def __getitem__(self, index):
+        img_name = str(self.annotations.iloc[index, 0])+".png" # Assuming image name is in the first column
+        img_path = os.path.join(self.image_folder, img_name)
+        image = Image.open(img_path).convert("RGB") # Load image and convert to RGB
+        target_str = self.annotations.iloc[index, 1] # Assuming target is in the second column
+        target = self.label2idx[target_str] 
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, target
+
